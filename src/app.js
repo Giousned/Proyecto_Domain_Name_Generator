eabf791037597b175b7390217e61818cb3205fee
Giousned <guille_ranza@hotmail.com>
2023-02-06 12:34:26 +0000

    [ADD] ejercidio resuelto de varias formas y con domain-hack incluido

--- src/app.js
+++ src/app.js
@@ -14,26 +14,58 @@ window.onload = function() {
   let extension = [".com", ".net", ".us", ".io"];
 
   let str = "";
+  let arr = [];
 
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
-        str += `${pronoun[i]}${adj[j]}${noun[k]}.com\n`;
+        let newArr = [];
+        newArr = [...noun[k]];
+
+        let lastLetter = newArr.pop();
+        let lastLetter2 = newArr.pop();
+
+        let newArr2 = newArr.join("");
+        str += `<li>${pronoun[i]}${adj[j]}${newArr2}.${lastLetter2}${lastLetter}</li>`;
       }
     }
   }
 
-  let arr = [];
-  arr = str;
-
-  // for (let l = 0; l < newArr.length; l++) {
-  //   let fila = document.querySelector("#tabla");
-  //   fila.innerHTML = newArr[l];
-  // }
+  arr = `<ol>${str}</ol>`;
 
   const imprimirHtml = document.querySelector("#imprimirTexto");
 
   return (imprimirHtml.innerHTML = arr);
-
-  // console.log("Hello Rigo from the console!");
 };
+
+// LISTA CON LAS EXTENSIONES RAMDON, CADA VEZ UNA DIFERENTE
+// for (let i = 0; i < pronoun.length; i++) {
+//   for (let j = 0; j < adj.length; j++) {
+//     for (let k = 0; k < noun.length; k++) {
+//       let randomNumber = Math.floor(Math.random() * extension.length);
+//       str += `<li>${pronoun[i]}${adj[j]}${noun[k]}${extension[randomNumber]}</li>`;
+//     }
+//   }
+// }
+
+// LISTA CON TODAS LAS EXTENSIONES POSIBLES
+// for (let i = 0; i < pronoun.length; i++) {
+//   for (let j = 0; j < adj.length; j++) {
+//     for (let k = 0; k < noun.length; k++) {
+//       for (let l = 0; l < extension.length; l++) {
+//         str += `<li>${pronoun[i]}${adj[j]}${noun[k]}${extension[l]}</li>`;
+//       }
+//     }
+//   }
+// }
+
+// LISTA AÑADIENDO A MANO (HARDCODEANDO) LA EXTENSION .COM
+// for (let i = 0; i < pronoun.length; i++) {
+//   for (let j = 0; j < adj.length; j++) {
+//     for (let k = 0; k < noun.length; k++) {
+//       str += `<li>${pronoun[i]}${adj[j]}${noun[k]}.com</li>`;
+//     }
+//   }
+// }
+
+// console.log("Hello Rigo from the console!");
