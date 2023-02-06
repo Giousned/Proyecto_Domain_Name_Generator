1e2c0529e40acf7ba7931689a54daefd357064bd
Giousned <guille_ranza@hotmail.com>
2023-02-06 20:28:34 +0000

    [ADD] hack domain and all results possibles

--- src/app.js
+++ src/app.js
@@ -8,35 +8,107 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
 
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let extension = [".com", ".net", ".us", ".io"];
+  const pronouns = ["the", "your", "every", "one", "our", "such"];
+  const adjectives = [
+    "great",
+    "adventurous",
+    "big",
+    "arrogant",
+    "charming",
+    "creepy",
+    "grotesque",
+    "hungry"
+  ];
+  const nouns = ["sitcom", "lotus", "cubes", "jogger", "racoon", "planet"];
+  const domains = [".com", ".us", ".es", ".io", ".net", ".shop"];
 
-  let str = "";
-  let arr = [];
+  let strList = "";
+  let arrOrdened = [];
 
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        let newArr = [];
-        newArr = [...noun[k]];
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        for (let l = 0; l < domains.length; l++) {
+          let strHackNouns = nouns[k];
+          let strHackDomains = domains[l].slice(1);
 
-        let lastLetter = newArr.pop();
-        let lastLetter2 = newArr.pop();
+          if (
+            strHackNouns.endsWith(strHackDomains) &&
+            strHackDomains.length === 2
+          ) {
+            let newArr = [];
+            newArr = [...nouns[k]];
 
-        let newArr2 = newArr.join("");
-        str += `<li>${pronoun[i]}${adj[j]}${newArr2}.${lastLetter2}${lastLetter}</li>`;
+            let lastLetter = newArr.pop();
+            let lastLetter2 = newArr.pop();
+
+            let newArr2 = newArr.join("");
+            strList += `<li>${pronouns[i]}${adjectives[j]}${newArr2}.${lastLetter2}${lastLetter}</li>`;
+          } else if (
+            strHackNouns.endsWith(strHackDomains) &&
+            strHackDomains.length === 3
+          ) {
+            let newArr = [];
+            newArr = [...nouns[k]];
+
+            let lastLetter = newArr.pop();
+            let lastLetter2 = newArr.pop();
+            let lastLetter3 = newArr.pop();
+
+            let newArr2 = newArr.join("");
+            strList += `<li>${pronouns[i]}${adjectives[j]}${newArr2}.${lastLetter3}${lastLetter2}${lastLetter}</li>`;
+          } else
+            strList += `<li>${pronouns[i]}${adjectives[j]}${nouns[k]}${domains[l]}</li>`;
+        }
       }
     }
   }
 
-  arr = `<ol>${str}</ol>`;
+  arrOrdened = `<ol>${strList}</ol>`;
 
   const imprimirHtml = document.querySelector("#imprimirTexto");
 
-  return (imprimirHtml.innerHTML = arr);
+  return (imprimirHtml.innerHTML = arrOrdened);
 };
+
+// for (let i = 0; i < pronouns.length; i++) {
+//   for (let j = 0; j < adjectives.length; j++) {
+//     for (let k = 0; k < nouns.length; k++) {
+//       for (let l = 0; l < domains.length; l++) {
+//         let strHackNouns = nouns[k];
+//         let strHackDomains = domains[l].slice(1);
+
+//         if (
+//           strHackNouns.endsWith(strHackDomains) &&
+//           strHackDomains.length === 2
+//         ) {
+//           let newArr = [];
+//           newArr = [...nouns[k]];
+
+//           let lastLetter = newArr.pop();
+//           let lastLetter2 = newArr.pop();
+
+//           let newArr2 = newArr.join("");
+//           strList += `<li>${pronouns[i]}${adjectives[j]}${newArr2}.${lastLetter2}${lastLetter}</li>`;
+//         } else if (
+//           strHackNouns.endsWith(strHackDomains) &&
+//           strHackDomains.length === 3
+//         ) {
+//           let newArr = [];
+//           newArr = [...nouns[k]];
+
+//           let lastLetter = newArr.pop();
+//           let lastLetter2 = newArr.pop();
+//           let lastLetter3 = newArr.pop();
+
+//           let newArr2 = newArr.join("");
+//           strList += `<li>${pronouns[i]}${adjectives[j]}${newArr2}.${lastLetter3}${lastLetter2}${lastLetter}</li>`;
+//         } else
+//           strList += `<li>${pronouns[i]}${adjectives[j]}${nouns[k]}${domains[l]}</li>`;
+//       }
+//     }
+//   }
+// }
 
 // LISTA CON LAS EXTENSIONES RAMDON, CADA VEZ UNA DIFERENTE
 // for (let i = 0; i < pronoun.length; i++) {
