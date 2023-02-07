eb8ccd99f113ba2564f9d5a20b2a0b1fdb3330d5
Giousned <guille_ranza@hotmail.com>
2023-02-07 10:36:21 +0000

    [ADD] metodo slice para hack domain, frase inicial y estilos bootstrap

--- src/app.js
+++ src/app.js
@@ -34,29 +34,20 @@ window.onload = function() {
 
           if (
             strHackNouns.endsWith(strHackDomains) &&
-            strHackDomains.length === 2
-          ) {
-            let newArr = [];
-            newArr = [...nouns[k]];
-
-            let lastLetter = newArr.pop();
-            let lastLetter2 = newArr.pop();
-
-            let newArr2 = newArr.join("");
-            strList += `<li>${pronouns[i]}${adjectives[j]}${newArr2}.${lastLetter2}${lastLetter}</li>`;
-          } else if (
-            strHackNouns.endsWith(strHackDomains) &&
             strHackDomains.length === 3
           ) {
             let newArr = [];
-            newArr = [...nouns[k]];
+            newArr = nouns[k].slice(0, -3);
 
-            let lastLetter = newArr.pop();
-            let lastLetter2 = newArr.pop();
-            let lastLetter3 = newArr.pop();
+            strList += `<li>${pronouns[i]}${adjectives[j]}${newArr}${domains[l]}</li>`;
+          } else if (
+            strHackNouns.endsWith(strHackDomains) &&
+            strHackDomains.length === 2
+          ) {
+            let newArr = [];
+            newArr = nouns[k].slice(0, -2);
 
-            let newArr2 = newArr.join("");
-            strList += `<li>${pronouns[i]}${adjectives[j]}${newArr2}.${lastLetter3}${lastLetter2}${lastLetter}</li>`;
+            strList += `<li>${pronouns[i]}${adjectives[j]}${newArr}${domains[l]}</li>`;
           } else
             strList += `<li>${pronouns[i]}${adjectives[j]}${nouns[k]}${domains[l]}</li>`;
         }
@@ -67,48 +58,34 @@ window.onload = function() {
   arrOrdened = `<ol>${strList}</ol>`;
 
   const imprimirHtml = document.querySelector("#imprimirTexto");
+  const mensajeHtml = document.querySelector("#mensaje");
+
+  mensajeHtml.innerHTML =
+    pronouns.length * adjectives.length * nouns.length * domains.length;
 
   return (imprimirHtml.innerHTML = arrOrdened);
 };
 
-// for (let i = 0; i < pronouns.length; i++) {
-//   for (let j = 0; j < adjectives.length; j++) {
-//     for (let k = 0; k < nouns.length; k++) {
-//       for (let l = 0; l < domains.length; l++) {
-//         let strHackNouns = nouns[k];
-//         let strHackDomains = domains[l].slice(1);
-
-//         if (
-//           strHackNouns.endsWith(strHackDomains) &&
-//           strHackDomains.length === 2
-//         ) {
+// LISTA CLONANDO EL ARRAY Y USANDO EL METODO DE POP PARA IR ELIMINANDO LAS ULTIMAS LETRAS
+//         if (strHackNouns.endsWith(strHackDomains) &&strHackDomains.length === 2) {
 //           let newArr = [];
 //           newArr = [...nouns[k]];
-
 //           let lastLetter = newArr.pop();
 //           let lastLetter2 = newArr.pop();
-
 //           let newArr2 = newArr.join("");
 //           strList += `<li>${pronouns[i]}${adjectives[j]}${newArr2}.${lastLetter2}${lastLetter}</li>`;
-//         } else if (
-//           strHackNouns.endsWith(strHackDomains) &&
-//           strHackDomains.length === 3
-//         ) {
+
+//         } else if (strHackNouns.endsWith(strHackDomains) &&strHackDomains.length === 3) {
 //           let newArr = [];
 //           newArr = [...nouns[k]];
-
 //           let lastLetter = newArr.pop();
 //           let lastLetter2 = newArr.pop();
 //           let lastLetter3 = newArr.pop();
-
 //           let newArr2 = newArr.join("");
 //           strList += `<li>${pronouns[i]}${adjectives[j]}${newArr2}.${lastLetter3}${lastLetter2}${lastLetter}</li>`;
+
 //         } else
 //           strList += `<li>${pronouns[i]}${adjectives[j]}${nouns[k]}${domains[l]}</li>`;
-//       }
-//     }
-//   }
-// }
 
 // LISTA CON LAS EXTENSIONES RAMDON, CADA VEZ UNA DIFERENTE
 // for (let i = 0; i < pronoun.length; i++) {
@@ -116,26 +93,6 @@ window.onload = function() {
 //     for (let k = 0; k < noun.length; k++) {
 //       let randomNumber = Math.floor(Math.random() * extension.length);
 //       str += `<li>${pronoun[i]}${adj[j]}${noun[k]}${extension[randomNumber]}</li>`;
-//     }
-//   }
-// }
-
-// LISTA CON TODAS LAS EXTENSIONES POSIBLES
-// for (let i = 0; i < pronoun.length; i++) {
-//   for (let j = 0; j < adj.length; j++) {
-//     for (let k = 0; k < noun.length; k++) {
-//       for (let l = 0; l < extension.length; l++) {
-//         str += `<li>${pronoun[i]}${adj[j]}${noun[k]}${extension[l]}</li>`;
-//       }
-//     }
-//   }
-// }
-
-// LISTA AÑADIENDO A MANO (HARDCODEANDO) LA EXTENSION .COM
-// for (let i = 0; i < pronoun.length; i++) {
-//   for (let j = 0; j < adj.length; j++) {
-//     for (let k = 0; k < noun.length; k++) {
-//       str += `<li>${pronoun[i]}${adj[j]}${noun[k]}.com</li>`;
 //     }
 //   }
 // }
